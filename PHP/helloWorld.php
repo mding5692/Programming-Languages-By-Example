c54c870c45d338efbfc76485146f0b27a3606203
Michael <m.ding5692@gmail.com>
2015-07-07 18:21:35 -0400

    Create helloWorld.php

---
+++ PHP/helloWorld.php
@@ -0,0 +1,10 @@
+<!-- PHP needs to be put on the client-side to display information -->
+
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> <!-- Could also use C-style syntax on command line -->
+ </body>
+</html>
