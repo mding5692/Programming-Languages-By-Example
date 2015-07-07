ec97e5229ce4bcc3c0a0d58e011d281be92edd7f
Michael <m.ding5692@gmail.com>
2015-07-07 18:31:41 -0400

    Create variables.nim

---
+++ Nim/variables.nim
@@ -0,0 +1,19 @@
+# Nim has three variable types: 
+# var for variable
+# const for constant
+# and let for variables whose values can be dynamically assigned but not changed aftewards
+# unlike constants which need to be assigned before compiling
+
+# strings
+
+var str = "string"
+
+const g = "constant"
+
+# this works too
+var 
+  a = 1
+  b = "hello"
+  c = 2
+  
+let d = "this is a let variable"
