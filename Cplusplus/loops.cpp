9c2fdd9bff9373d7c7fd75cea829674ad741b7a6
Michael <m.ding5692@gmail.com>
2015-07-07 18:47:20 -0400

    Create loops.cpp

---
+++ Cplusplus/loops.cpp
@@ -0,0 +1,28 @@
+// illustrating the for, do and while loops
+
+int main() {
+
+  int i = 0;
+  // while loop
+  while ( i < 10 ) {
+  
+    i++;
+    }
+  
+  int sum = 0;
+  // for loop
+  for (int y = 0; y < 10; y++) {
+   sum += y;
+   }
+   
+  int j = 0; 
+  // do-while loop
+  do {
+    
+    j++;
+    
+  } while ( j < 20)
+    
+  return 0;
+  
+} // its pretty familiar for programmers, this is more for beginners
