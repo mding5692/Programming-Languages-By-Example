ddb832343b465b4e47644b540b45bf12a6830bab
Michael <m.ding5692@gmail.com>
2015-07-07 18:28:55 -0400

    Create helloWorld.nim

---
+++ Nim/helloWorld.nim
@@ -0,0 +1,13 @@
+# UNIX command line
+# Comments look like this
+echo "Hello World"
+
+# How to compile:
+
+# nim c -r --verbosity:0 helloworld.nim
+
+# c is for compile 
+
+# -r is for run
+
+# --verbosity:0 is for only printing the "Hello World" message, as by default some debug messages would also pop out
