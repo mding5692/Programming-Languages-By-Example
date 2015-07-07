280f275a5c86a15901fbc5e93bf309726afa8ebb
Michael <m.ding5692@gmail.com>
2015-07-07 18:22:54 -0400

    Create helloWorld.cr

---
+++ Crystal/helloWorld.cr
@@ -0,0 +1,7 @@
+# crystal language is still in early development, only supports one-line comments
+
+# pretty simple to print out output
+
+puts "Hello World"
+
+# for all them ruby developers, this is very familiar syntax
