a1a136b5632eea2c4c00af8d9ed4afd36f7bb798
Michael <m.ding5692@gmail.com>
2015-07-07 18:46:02 -0400

    Create helloWorld.cpp

---
+++ Cplusplus/helloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // header for required service for I/O
+
+using namespace std; /* the directive for when standard libraries are used */
+
+int main() // main method 
+{
+  cout << "Hello World" << endl;  /* cout for output, endl for end of line */
+  return 0; // c always needs this to end
+}
