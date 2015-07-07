8d671e4cf8279b0a3fbd7122966b4bd51483c258
Michael <m.ding5692@gmail.com>
2015-07-07 18:48:16 -0400

    Create switch.cpp

---
+++ Cplusplus/switch.cpp
@@ -0,0 +1,14 @@
+// illustrating a switch statement in c++
+
+int main() {
+
+int number = 2;
+  
+switch(number) {
+  case 1: numberName = 1; break; // if number == 1 then numberName = 1
+  case 2: numberName = 2; break; // if number == 2 then numberName = 2
+}
+
+return 0;
+
+}
