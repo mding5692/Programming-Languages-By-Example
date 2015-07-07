3e37c12a910a92bcf05bea3a63c912706f63de28
Michael <m.ding5692@gmail.com>
2015-07-07 18:25:24 -0400

    Create helloWorld.cs

---
+++ Csharp/helloWorld.cs
@@ -0,0 +1,13 @@
+// Surprise, surprise, c# follows c-style comments
+
+/* Looks like you have to call on a System library to print stuff out on the console */
+
+using System;
+public class HelloWorld
+  {
+    // reminds me of Java main function
+    public static void Main(string[] args) 
+    {
+      Console.Write("Hello World");
+    }
+  }
