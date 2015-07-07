e5c0936d437314e36e188992e82e47c9f16881cf
Michael <m.ding5692@gmail.com>
2015-07-07 18:24:29 -0400

    Create helloWorld.vb

---
+++ Visual-Basic/helloWorld.vb
@@ -0,0 +1,20 @@
+' Interesting way of writing comments
+' theres two ways of doing this, one in a messageBox, another to the console
+
+' console format
+Import System
+
+Public Module modmain
+  Sub Main()
+    Console.WriteLine("Hello World!")
+  End Sub
+End Module
+
+' messagebox format
+Module helloWorld
+  ' main method
+  Sub Main() 
+    ' guess I'm using Visual Basic API
+    MsgBox("Hello World")
+  End Sub
+End Module
