e7adb260307e357fbdfd620cd4f12085fc30857b
Michael <m.ding5692@gmail.com>
2015-07-07 18:27:20 -0400

    Create helloWorld.rs

---
+++ Rust/helloWorld.rs
@@ -0,0 +1,14 @@
+// Looks like it still uses c-style comments
+
+/* Multi-line comments are c-style too
+    and looks like we don't need any extra dependencies
+    to print out text, seems like its built in.
+*/
+
+
+fn main() {
+
+  /** notice the exclamation mark aftet println **/
+  println!("Hello World);
+  
+}
