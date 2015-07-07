160317e4d41568c7847d81943d84a719306bec3f
Michael <m.ding5692@gmail.com>
2015-07-07 18:48:01 -0400

    Create ifElse.cpp

---
+++ Cplusplus/ifElse.cpp
@@ -0,0 +1,18 @@
+/* standard if else decision */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int i = 0;
+// if and else statement
+if ( i == 0 ) {
+  cout << "i is zero" << endl;
+  } else {
+  cout << "i isn't zero" << endl;
+  }
+
+return 0;
+}
