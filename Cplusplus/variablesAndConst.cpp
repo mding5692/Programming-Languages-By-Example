9c2b982947206e8607cd64b2dbeb31a80b13a7ec
Michael <m.ding5692@gmail.com>
2015-07-07 18:46:50 -0400

    Create variablesAndConst.cpp

---
+++ Cplusplus/variablesAndConst.cpp
@@ -0,0 +1,28 @@
+// variable names cannot start with a number, have spaces or use a symbol or use a reserved word
+
+#include <iostream>
+
+using namespace std;
+
+/* Number data types include:
+  int is an integer whereas double has decimal places
+  */
+  
+  int main() {
+  
+  int integerNum = 5;
+  double doubleNum = 1.0;
+  // this over here is an constant
+  const int INTEGERCONSTANT = 1;
+  // which stays the same value when initialized
+  
+  // strings as character arrays 
+  string str = "this is a string";
+  // can't concatenate two string literals in c++
+  
+  // booleans
+  bool thisIsTrue = true;
+  
+  return 0;
+  }
+  
