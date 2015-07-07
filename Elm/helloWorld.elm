fc022b83f971839d7c45ea33a5afa24a60a51afe
Michael <m.ding5692@gmail.com>
2015-07-07 18:30:59 -0400

    Create helloWorld.elm

---
+++ Elm/helloWorld.elm
@@ -0,0 +1,10 @@
+-- This is an elm comment
+
+{- Looks like to print something out, you 
+would need the Text library -}
+
+import Text 
+
+-- Interesting way of declaring the main function 
+-- also calls on the Text library to deliver something into plainText format
+main = Text.plainText "Hello World"
