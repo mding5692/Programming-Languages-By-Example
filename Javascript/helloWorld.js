d7408ddd532956f2e4e97287518dcde925133f2a
Michael <m.ding5692@gmail.com>
2015-07-07 18:39:33 -0400

    Create helloWorld.js

---
+++ Javascript/helloWorld.js
@@ -0,0 +1,8 @@
+// for the console
+
+console.log("hello world");
+
+/* for the html web pages */
+alert("Hello World") // pop-up window style
+
+document.write("hello world"); // or just on the DOM
