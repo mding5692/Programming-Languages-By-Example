57f8fa7f69618938c1b36074ba517d5e960401c9
Michael <m.ding5692@gmail.com>
2015-07-07 18:38:28 -0400

    Create helloWorld.java

---
+++ Java/helloWorld.java
@@ -0,0 +1,12 @@
+// Still most familiar with java
+
+public class HelloWorld {
+  
+  /* Main function and standard boilerplate code */
+  public static void main(String[] args) {
+    // prints out in the console
+    System.out.println("Hello, World");
+  
+  }
+  
+}
