8703b8a590a0b55fc37129c47074f865215d2b02
Michael <m.ding5692@gmail.com>
2015-07-07 18:26:27 -0400

    Create helloWorld.rb

---
+++ Ruby/helloWorld.rb
@@ -0,0 +1,9 @@
+# This is an one-line comment
+
+=begin 
+  
+  I like their multi-line comments, pretty interesting to use,
+  as well creating and printing some output seems very easy as well
+=end
+
+puts "Hello World"
