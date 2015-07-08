7f1cca5a743c35d4755e6fddfe0cf6b0a78f1a1c
Michael <m.ding5692@gmail.com>
2015-07-07 22:46:58 -0400

    Create varsAndConsts.js

---
+++ Javascript/varsAndConsts.js
@@ -0,0 +1,10 @@
+/* don't need to declare what type of variable
+just the reserved word var is enough */
+
+var integer = 1;
+var str = "hello";
+
+var boole = true;
+
+// same for constants
+const one = 1;
