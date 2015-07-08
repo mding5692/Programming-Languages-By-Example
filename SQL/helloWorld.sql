54aa6c1e33a865a84041b22fff7657c2f27ec90d
Michael <m.ding5692@gmail.com>
2015-07-07 22:43:45 -0400

    Create helloWorld.sql

---
+++ SQL/helloWorld.sql
@@ -0,0 +1,6 @@
+# this is a comment
+/* so is this */
+
+SELECT "hello world" // not that it does much
+
+SELECT 'Hello, World!' FROM dual; // capital letters aren't really needed, just for illustration and also from dual is needed for oracle DBMS 
