98a2b5e76569e58be0d3b834911228d582e99014
Michael <m.ding5692@gmail.com>
2016-04-16 10:26:28 -0400

    Create helloWorld.swift

---
+++ Swift/helloWorld.swift
@@ -0,0 +1,15 @@
+// This is a comment
+
+/* This is a comment
+as well
+*/
+
+// variables
+var helloWorld = "helloo World...";
+
+// constants that are final value
+let helooo = "hello";
+
+// prints it out
+print(helloWorld);
+print(helooo);
